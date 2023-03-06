545627cf490f95b0d916525cace63757c85ba7e1
kozraf <kozraf@gmail.com>
2023-03-06 19:42:50 +0000

    Adding metrics-server script

--- K8-metrics_server/install.sh
+++ K8-metrics_server/install.sh
@@ -1,2 +1,6 @@
+#https://blog.zespre.com/deploying-metrics-server-on-kubernetes-cluster-installed-with-kubeadm.html
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade --install metrics-server metrics-server/metrics-server
+kubectl get po -A
+
 
