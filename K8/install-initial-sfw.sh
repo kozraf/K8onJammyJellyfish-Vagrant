4272c0df81be07f9c26fe108e8174298377ffe44
kozraf <kozraf@gmail.com>
2023-03-11 14:55:20 +0000

    renaming install-docker.sh

--- K8/install-initial-sfw.sh
+++ K8/install-initial-sfw.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Install Docker
+# Install initial software
 echo -e "--------"
-echo -e "----Install Docker----"
+echo -e "----Install initial software----"
 sudo apt-get update
 sudo apt upgrade -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates containerd.io -y
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates containerd.io jq -y
