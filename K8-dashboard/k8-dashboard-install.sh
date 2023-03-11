986ad74b175c4ccf0b1926089ba8c23c59773134
kozraf <kozraf@gmail.com>
2023-03-11 15:45:34 +0000

    add animation to k8-dashboard-install.sh

--- K8-dashboard/k8-dashboard-install.sh
+++ K8-dashboard/k8-dashboard-install.sh
@@ -9,6 +9,32 @@ mkdir /home/vagrant/K8-dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 kubectl -n kubernetes-dashboard patch service kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":32321}]'
 
+#!/bin/bash
+
+# Define the animation
+animation="-\|/"
+
+while true; do
+  for ns in $(kubectl get namespaces -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'); do
+    for pod in $(kubectl get pods -n $ns -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'); do
+      total=$(kubectl get pod $pod -n $ns -o=jsonpath='{range .spec.containers[*]}{.name}{"\n"}{end}' | wc -l)
+      running=$(kubectl get pod $pod -n $ns -o=jsonpath='{range .status.containerStatuses[*]}{.state.running}{end}' | grep -c true)
+      if [[ $total -ne $running ]]; then
+        for i in $(seq 0 3); do
+          echo -ne "\r[${animation:$i:1}]"
+          sleep 0.1
+          done
+        echo -e "\033[33m---\033[0m"
+        echo -e "\033[33mWaiting for all containers to be running in pod $pod in namespace $ns \033[0m"
+        sleep 1
+      fi
+    done
+  done
+  echo -e "\e[32mAll pods are ready!\e[0m"
+  break
+done
+
+
 # Create admin-user Service Account
 sudo tee /home/vagrant/K8-dashboard/k8-sa_admi-user.yaml <<EOF
 apiVersion: v1
@@ -40,6 +66,6 @@ kubectl apply -f /home/vagrant/K8-dashboard/k8dash-cr-crb_admin-user.yaml
 echo -e "!!!"
 kubectl -n kubernetes-dashboard create token admin-user
 echo -e "!!!"
-echo "Copy above between !!! but remember that output is wrapped!"
+echo -e "\e[32mCopy above between !!! but remember that output is wrapped!!\e[0m"
 
 
